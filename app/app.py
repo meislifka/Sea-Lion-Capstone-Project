bb74fcafc0e74e47089dda1ef713dd3d7875b20c
torkfat <105743305+torkfat@users.noreply.github.com>
2022-10-24 12:38:42 -0400

    Update app.py
    Add database stuff

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request
 import sqlite3
 app = Flask(__name__)
+con = sqlite3.connect("sealions.db")
+db = con.cursor()
+db.execute("CREATE TABLE sealions(name, age)")
+db.execute("INSERT INTO sealions(name, age) VALUES('John', '21')")
+con.commit()
+info = db.execute("SELECT name FROM sealions")
+info = info.fetchall()
 @app.route('/')
 def home():
     return render_template('home.html')
