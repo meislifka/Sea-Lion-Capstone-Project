afd786aada9bcbcf90f7e36540e89150413cac63
meislifka <99206307+meislifka@users.noreply.github.com>
2022-10-24 12:21:10 -0400

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
